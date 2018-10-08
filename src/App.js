ab9a069cddd7971f2f21c289224796ecbff93410
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2018-10-07 23:47:00 -0300

    ✨ Add stepper to the initial page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,109 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { mainListItems } from './MenuList';
+import { styles } from './App.style';
 
-import Menu from './Menu/Menu';
+import StartPage from './StartPage/StartPage';
+import AppStepper from './AppStepper/AppStepper';
 
-class App extends Component {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      files: []
+    };
+  }
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
+  updateFiles = (files) => {
+    this.setState({files});
+  }
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="App">
-        <Menu/>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div className={classes.root}>
+          <AppBar
+            position="absolute"
+            className={classNames(classes.appBar, this.state.open && classes.appBarShift)}
+          >
+            <Toolbar disableGutters={!this.state.open} className={classes.toolbar}>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerOpen}
+                className={classNames(
+                  classes.menuButton,
+                  this.state.open && classes.menuButtonHidden,
+                )}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                component="h1"
+                variant="title"
+                color="inherit"
+                noWrap
+                className={classes.title}
+              >
+                miRQuest 2
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            variant="permanent"
+            classes={{
+              paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+            }}
+            open={this.state.open}
+          >
+            <div className={classes.toolbarIcon}>
+              <IconButton onClick={this.handleDrawerClose}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </div>
+            <Divider />
+            <List>{mainListItems}</List>
+          </Drawer>
+          <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            {/* <StartPage files={this.state.files} updateFiles={(files) => this.updateFiles(files)}/> */}
+            <AppStepper/>
+          </main>
+        </div>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
