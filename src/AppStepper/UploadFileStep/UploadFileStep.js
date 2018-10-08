8a52f74ec05d5e06725491af82f2c24c30fb4e20
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2018-10-07 23:46:08 -0300

    ✨ Add UploadFileStep component

---
+++ src/AppStepper/UploadFileStep/UploadFileStep.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Dropzone from 'react-dropzone';
+
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import './UploadFileStep.css';
+import { uploadFileStepStyles } from './UploadFileStep.style';
+
+
+class UploadFileStep extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      files: []
+    }
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="start-page">
+        <Typography variant="body1" gutterBottom component="span"> Please choose your FASTA files to be analyzed</Typography>
+        <div>
+          <div className="dropzone">
+            <Dropzone onDrop={this.onDrop.bind(this)} className={classes.dropzone} accept=".fa">
+              <span>Try dropping some FASTA files here, or click to select files to upload.</span>
+            </Dropzone>
+          </div>
+          <aside>
+            <h2>Dropped files</h2>
+            <ul>
+              {
+                this.state.files.map(f => <li>{f.name} - {f.size} bytes</li>)
+              }
+            </ul>
+          </aside>
+        </div>
+      </div>
+    );
+  }
+
+  onDrop(files) {
+    this.setState({files});
+    // this.props.updateFiles(files);
+  }
+}
+
+UploadFileStep.propTypes = {
+  classes: PropTypes.object.isRequired,
+  files: PropTypes.array,
+  updateFiles: PropTypes.func,
+};
+
+export default withStyles(uploadFileStepStyles)(UploadFileStep);
---
+++ src/AppStepper/UploadFileStep/UploadFileStep.style.js
@@ -0,0 +1,14 @@
+export const uploadFileStepStyles = theme => ({
+  dropzone: {
+    width: '100%',
+    height: 300,
+    padding: 15,
+    borderWidth: 2,
+    borderColor: 'rgb(102, 102, 102)',
+    borderStyle: 'dashed',
+    borderRadius: 5,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
