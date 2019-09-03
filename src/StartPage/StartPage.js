3e5f59755b804f543eb8438d522ea0439b491f7a
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2019-09-03 10:06:59 -0300

    ✨Update StartPage component

--- src/StartPage/StartPage.js
+++ src/StartPage/StartPage.js
@@ -3,17 +3,43 @@ import PropTypes from 'prop-types';
 
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import TimelineIcon from '@material-ui/icons/Timeline';
+import BarChartIcon from '@material-ui/icons/BarChart';
+
 import './StartPage.css';
 import { startPageStyles } from './StartPage.style';
+
+import { Link } from "react-router-dom";
 
 
 class StartPage extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className="start-page">
-        <Typography variant="display1" gutterBottom> Welcome to miRQuest 2</Typography>
-      </div>
+        <Grid container spacing={16} className={classes.root}>
+          <Grid item xs={12}>
+            <Typography variant="display1" gutterBottom> Welcome to miRQuest 2</Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paperContainer}>
+              <Button className="button-home-page" component={Link} to="/process">
+                <TimelineIcon />
+                Process your data
+              </Button>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paperContainer}>
+              <Button className="button-home-page" component={Link} to="/report">
+                <BarChartIcon />
+                Check a report
+              </Button>
+            </Paper>
+          </Grid>
+        </Grid>
     );
   }
 
--- src/StartPage/StartPage.style.js
+++ src/StartPage/StartPage.style.js
@@ -1,3 +1,12 @@
 export const startPageStyles = theme => ({
-
+    root: {
+        flexGrow: 1,
+        height: "100%",
+    },
+    paperContainer: {
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    }
 });
