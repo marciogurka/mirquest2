ae01a55b99d08ae0d509fa9dd1fb21263daebd6a
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2020-08-15 19:04:37 -0300

    ✨Add theme support for the application
    Following the Material-UI theme provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@material-ui/styles';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import theme from './theme';
 import Store from '~/store';
 
 ReactDOM.render(
   <Provider store={Store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#00838f'
+    },
+    secondary: {
+      main: '#ad1457'
+    }
+  }
+});
+
+export default theme;
