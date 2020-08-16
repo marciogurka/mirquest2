bf9d03e6781d6a577d4cb91467cca2bd0763fd96
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2020-08-16 00:04:45 -0300

    🎨Add ProcessFile component style

---
+++ src/components/ProcessFile/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Grid } from '@material-ui/core';
+
+export const FullHeightGrid = styled(Grid)`
+  display: flex;
+  align-items: stretch;
+  padding: 15px;
+`;
