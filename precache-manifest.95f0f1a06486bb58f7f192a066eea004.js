bbafdb9546da133dfc1d138a8bdedf88370966a3
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2020-08-17 11:34:50 -0300

    Updates

---
+++ precache-manifest.95f0f1a06486bb58f7f192a066eea004.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df217c5228e4ac8b897f04d899858365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb2a823d9591f8043793",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "4e2982b4595af1f6b6de",
+    "url": "/static/css/main.305666cb.chunk.css"
+  },
+  {
+    "revision": "fb2a823d9591f8043793",
+    "url": "/static/js/2.482b0f62.chunk.js"
+  },
+  {
+    "revision": "43d72dab81fe6139b99e36ca2e0cb538",
+    "url": "/static/js/2.482b0f62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2982b4595af1f6b6de",
+    "url": "/static/js/main.aee9cddd.chunk.js"
+  },
+  {
+    "revision": "d2f9d75d2f8607ef6a12",
+    "url": "/static/js/runtime-main.743d75d5.js"
+  }
+]);
